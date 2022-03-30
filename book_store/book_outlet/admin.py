b44c8c57c1d2a093200afff15d24f34d61c60d6f
littl3_R <arch@arch.com>
2022-03-30 19:04:47 +0000

    New relationships
    1. one-to-one
    2. one-to-many
    3. many-to-many

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book, Address, Country
 
 # Register your models here.
 
@@ -19,3 +19,6 @@ class BookAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
