83bd5cebce5b498f647196797246cb446cd059d1
littl3_R <arch@arch.com>
2022-04-04 21:18:11 +0000

    Added static images files

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,20 +1,23 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
 
-def store_file(file):
-    with open("temp/image.png", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
-
-    def post(self, request):
-        store_file(request.FILES["image"])
-        return HttpResponseRedirect("/profiles")
+class ProfilesViews(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
