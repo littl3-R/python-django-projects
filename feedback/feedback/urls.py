6756a9845c08127c441b210c0cfc509f05474f5a
littl3_R <arch@arch.com>
2022-04-04 19:52:49 +0000

    First attempt to upload file

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("reviews.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("reviews.urls")),
+    path("profiles/", include("profiles.urls")),
+]
