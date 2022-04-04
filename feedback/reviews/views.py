3f991806617cb9ade728e1dd45d194d260a502d0
littl3_R <arch@arch.com>
2022-04-04 19:04:32 +0000

    Cleaned up the code

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,16 +1,8 @@
-from django.views import View
-
-from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 
 from .models import Review
-
-# from .forms import ReviewForm
-
-# Create your views here.
 
 
 class ReviewView(CreateView):
