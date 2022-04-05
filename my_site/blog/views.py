722db0110cc4c1fdb56914dc0496c24ec2d9c839
littl3_R <arch@arch.com>
2022-04-05 17:33:08 +0000

    Created new session read later improved the forms and the views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,6 +1,10 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls.base import reverse
 from .models import Post
 from django.views.generic import ListView, DetailView
+from django.views import View
+from .forms import CommentForm
 
 
 class StartingPageView(ListView):
@@ -22,11 +26,61 @@ class AllPostView(ListView):
     context_object_name = "all_posts"
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+        }
+        return render(request, "blog/post-detail.html", context)
+
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_id = int(request.POST["post_id"])
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+            request.session["stored_posts"] = stored_posts
+
+        return HttpResponseRedirect("/")
