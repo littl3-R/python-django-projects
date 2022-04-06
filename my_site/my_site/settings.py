f5e33736508f3247d0982ab5c6efcbb189fd96c4
littl3_R <arch@arch.com>
2022-04-06 19:31:04 +0000

    Create first step to deployed in AWS

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-mhp@vzw1t$!1=qzc%!cpu@15-mw4uc8!+ftz59-8g1%197k8w="
+# SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [getenv("APP_HOST")]
 
 # Application definition
 
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
