e9fd3c003935fc81b8de2eeeb2e19cf1542c86d5
littl3_R <arch@arch.com>
2022-04-06 17:10:03 +0000

    Created a "Read Later" with sessions

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -27,13 +27,24 @@ class AllPostView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
             "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -52,6 +63,7 @@ class SinglePostView(View):
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
             "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id),
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -81,6 +93,9 @@ class ReadLaterView(View):
         post_id = int(request.POST["post_id"])
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
 
         return HttpResponseRedirect("/")
